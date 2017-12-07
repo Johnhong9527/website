eb407c062245332c8921f0b8f1cf9cfd16de0f37
seam <honghaitzz11@gmail.com>
2017-12-07 14:47:22 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 function init() {
-
   var tru = true;
   var DOMTop = document.getElementById('nav');
   var cLog = document.getElementById('cLog');
+  // deBug
+  var deBug = false;
+  if (deBug) {
+    cLog.style.display = 'block';
+  }
+
   document.addEventListener('scroll', function () {
     var clientHeight = document.documentElement.clientHeight;
-    var scrollTop = document.body.scrollTop;
+    // var scrollTop = document.body.scrollTop;
+    // var scrollTop = document.documentElement.scrollTop;
+    var scrollTop = null;
+    if (document.body.scrollTop !== 0) {
+      scrollTop = document.body.scrollTop
+    } else {
+      scrollTop = document.documentElement.scrollTop
+    }
     // console.log(scrollTop.scrollTop)
     cLog.innerHTML = `DOMTop.offsetTop:${DOMTop.offsetTop};<br/>clientHeight: ${clientHeight};<br/>scrollTop:${scrollTop};`;
     if (tru && clientHeight + DOMTop.offsetTop + DOMTop.clientHeight < clientHeight + scrollTop) {
