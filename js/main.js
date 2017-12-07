e38038ac82ee65884ea4adb3c57ff807d92fde3f
seam <honghaitzz11@gmail.com>
2017-12-07 17:28:30 +0800

    开始标题动画制作

--- js/main.js
+++ js/main.js
@@ -9,7 +9,26 @@ function init() {
   }
   /*debug*/
   // cLog.innerHTML
-
+  setTimeout(function () {
+    var animateIn = document.querySelectorAll('.container .title strong');
+    // console.log(animateIn.getAttribute('class'))
+    // console.log(animateIn)
+    var that = null;
+    for (let i = 0; i < animateIn.length; i++) {
+      animateIn[i].addEventListener('click', function () {
+        if (that !== null) {
+          that.removeAttribute('class');
+          if (that === this) {
+            this.removeAttribute('class');
+            that = null;
+            return;
+          }
+        }
+        this.setAttribute('class', 'animate-in');
+        that = this;
+      })
+    }
+  }, 10)
   document.addEventListener('scroll', function () {
     var clientHeight = document.documentElement.clientHeight;
     var scrollTop = null;
@@ -23,7 +42,7 @@ function init() {
       DOMTop.setAttribute('class', 'page-nav top');
       tru = false
     }
-    if (clientHeight + DOMTop.offsetTop + DOMTop.clientHeight > clientHeight + scrollTop) {
+    if (clientHeight + DOMTop.offsetTop + DOMTop.clientHeight - 100 > clientHeight + scrollTop) {
       DOMTop.setAttribute('class', 'page-nav');
       tru = true
     }
