70004346162bbc39bad930f459b1a38b4c6f815b
seam <honghaitzz11@gmail.com>
2017-12-07 14:29:58 +0800

    1

--- js/main.js
+++ js/main.js
@@ -6,9 +6,7 @@ function init() {
   document.addEventListener('scroll', function () {
     var clientHeight = document.documentElement.clientHeight;
     var scrollTop = document.documentElement.scrollTop;
-    cLog.innerHTML = 'clientHeight: ' + clientHeight +
-      ';<br/>scrollTop:' + scrollTop +
-      ';<br/>DOMTop.offsetTop + DOMTop.clientHeight: ' + (DOMTop.offsetTop + DOMTop.clientHeight) + ';<br/>clientHeight + scrollTop: ' + (clientHeight + scrollTop);
+    cLog.innerHTML = `DOMTop.offsetTop:${DOMTop.offsetTop};<br/>clientHeight: ${clientHeight};<br/>scrollTop:${scrollTop};`;
     if (tru && clientHeight + DOMTop.offsetTop + DOMTop.clientHeight < clientHeight + scrollTop) {
       console.log('发现元素')
       DOMTop.setAttribute('class', 'page-nav top');
