614f60217211c79b292bbd82a53e55743b554748
seam <honghaitzz11@gmail.com>
2017-12-07 15:16:14 +0800

    开始标题动画制作

--- js/main.js
+++ js/main.js
@@ -7,26 +7,23 @@ function init() {
   if (deBug) {
     cLog.style.display = 'block';
   }
+  /*debug*/
+  // cLog.innerHTML
 
   document.addEventListener('scroll', function () {
     var clientHeight = document.documentElement.clientHeight;
-    // var scrollTop = document.body.scrollTop;
-    // var scrollTop = document.documentElement.scrollTop;
     var scrollTop = null;
+    console.log(document.documentElement.scrollLeft)
     if (document.body.scrollTop !== 0) {
       scrollTop = document.body.scrollTop
     } else {
       scrollTop = document.documentElement.scrollTop
     }
-    // console.log(scrollTop.scrollTop)
-    cLog.innerHTML = `DOMTop.offsetTop:${DOMTop.offsetTop};<br/>clientHeight: ${clientHeight};<br/>scrollTop:${scrollTop};`;
     if (tru && clientHeight + DOMTop.offsetTop + DOMTop.clientHeight < clientHeight + scrollTop) {
-      console.log('发现元素')
       DOMTop.setAttribute('class', 'page-nav top');
       tru = false
     }
     if (clientHeight + DOMTop.offsetTop + DOMTop.clientHeight > clientHeight + scrollTop) {
-      // DOMTop.removeAttribute('class');
       DOMTop.setAttribute('class', 'page-nav');
       tru = true
     }
