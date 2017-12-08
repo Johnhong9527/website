186aa0432b3542b407f12a7ba29661f5e082cf9d
seam <honghaitzz11@gmail.com>
2017-12-08 15:59:36 +0800

    添加锚链接跳转过度效果

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ function init() {
 
   // 导航条动画，设定
   function DomScroll() {
-    let DOMTop = document.getElementById('nav'); // 获取导航对象
-    let clientHeight = document.documentElement.clientHeight; // 获取窗口可视区域高度
-    let tru = true;  // 是否替换类名的依据
-    let clientHeightScrollTop = clientHeight + DOMTop.offsetTop + DOMTop.clientHeight; // 是否替换类名的依据
+    let DOMTop = document.getElementById('nav'), // 获取导航对象
+      clientHeight = document.documentElement.clientHeight, // 获取窗口可视区域高度
+      tru = true,  // 是否替换类名的依据
+      clientHeightScrollTop = clientHeight + DOMTop.offsetTop + DOMTop.clientHeight; // 是否替换类名的依据
     // 监听文档滚动条事件，绑定动画
     document.addEventListener('scroll', function () {
       let scrollTop = null;
@@ -26,11 +26,15 @@ function init() {
       }
     }, false);
   }
+
   // deBug
   let deBug = false;
   deBugF(deBug);
+
   function deBugF(deBug) {
-    if(!deBug){return;}
+    if (!deBug) {
+      return;
+    }
     console.log('deBug')
     let cLog = document.getElementById('cLog'); // 错误信息加载
 
@@ -63,12 +67,44 @@ function init() {
     }
   }, 10)
 
-  $('.animation-top a').click(function () {
-    alert('a');
+  let aTag = document.querySelectorAll('.animation-top a');
+  for (let i = 0; i < aTag.length; i++) {
+    aTag[i].addEventListener('click', function () {
+      var $target = document.getElementById(this.hash.slice(1));
+      scrollToTop($target.offsetTop);
+
+      function scrollToTop(scrollDuration) {
+        var scrollTop = null,
+          scrollStep = null,
+          s = 0;
+        if (document.body.scrollTop !== 0) {
+          scrollTop = document.body.scrollTop
+        } else {
+          scrollTop = document.documentElement.scrollTop
+        }
+        scrollStep = scrollTop = Number.parseInt(scrollTop);
+        console.log('滚动前，滚动条位置:' + scrollTop);
+        console.log('目标位置：' + scrollDuration);
+        var scrollInterval = setInterval(function () {
+          if (s < 26) {
+            window.scrollTo(0, scrollStep + (scrollDuration - scrollTop) / 25 * s - 20);
+            s++;
+          }
+          else {
+            clearInterval(scrollInterval);
+            scrollStep = null;
+          }
+        }, 15);
+      }
+
+    }, false);
+  }
+
+  /*$('.animation-top a').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       let $target = $(this.hash);
-      console.log($(this.hash))
       $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+      console.log(this.hash.slice(1));
       if ($target.length) {
         let targetOffset = $target.offset().top;
         $('html,body').animate({
@@ -78,7 +114,8 @@ function init() {
         return false;
       }
     }
-  });
+  });*/
 }
+
 
 window.onload = init;
