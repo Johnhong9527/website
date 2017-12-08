43c790b867a0019930b2b6324d8884f2c3f03d51
seam <honghaitzz11@gmail.com>
2017-12-08 11:16:08 +0800

    添加若干功能

--- js/main.js
+++ js/main.js
@@ -1,19 +1,52 @@
 function init() {
-  var tru = true;
-  var DOMTop = document.getElementById('nav');
-  var cLog = document.getElementById('cLog');
-  // deBug
-  var deBug = false;
-  if (deBug) {
-    cLog.style.display = 'block';
+  // 导航条动画,初始化
+  DomScroll();
+
+  // 导航条动画，设定
+  function DomScroll() {
+    let DOMTop = document.getElementById('nav'); // 获取导航对象
+    let clientHeight = document.documentElement.clientHeight; // 获取窗口可视区域高度
+    let tru = true;  // 是否替换类名的依据
+    let clientHeightScrollTop = clientHeight + DOMTop.offsetTop + DOMTop.clientHeight; // 是否替换类名的依据
+    // 监听文档滚动条事件，绑定动画
+    document.addEventListener('scroll', function () {
+      let scrollTop = null;
+      if (document.body.scrollTop !== 0) {
+        scrollTop = document.body.scrollTop
+      } else {
+        scrollTop = document.documentElement.scrollTop
+      }
+      if (tru && clientHeightScrollTop < clientHeight + scrollTop) {
+        DOMTop.setAttribute('class', 'page-nav top');
+        tru = false
+      }
+      if (clientHeightScrollTop > clientHeight + scrollTop) {
+        DOMTop.setAttribute('class', 'page-nav');
+        tru = true
+      }
+    }, false);
   }
-  /*debug*/
-  // cLog.innerHTML
+  // deBug
+  let deBug = false;
+  deBugF(deBug);
+  function deBugF(deBug) {
+    if(!deBug){return;}
+    console.log('deBug')
+    let cLog = document.getElementById('cLog'); // 错误信息加载
+
+    if (deBug) {
+      cLog.style.display = 'block';
+    }
+    /*debug*/
+    // cLog.innerHTML
+  }
+
+  // 异步执行文章标题的动画
   setTimeout(function () {
-    var animateIn = document.querySelectorAll('.container .title strong');
+    let animateIn = document.querySelectorAll('.container .title strong');
     // console.log(animateIn.getAttribute('class'))
     // console.log(animateIn)
-    var that = null;
+    let that = null;
     for (let i = 0; i < animateIn.length; i++) {
       animateIn[i].addEventListener('click', function () {
         if (that !== null) {
@@ -29,24 +62,22 @@ function init() {
       })
     }
   }, 10)
-  document.addEventListener('scroll', function () {
-    var clientHeight = document.documentElement.clientHeight;
-    var scrollTop = null;
-    console.log(document.documentElement.scrollLeft)
-    if (document.body.scrollTop !== 0) {
-      scrollTop = document.body.scrollTop
-    } else {
-      scrollTop = document.documentElement.scrollTop
+
+  $('.animation-top').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      let $target = $(this.hash);
+      console.log($(this.hash))
+      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+      if ($target.length) {
+        let targetOffset = $target.offset().top;
+        $('html,body').animate({
+            scrollTop: targetOffset
+          },
+          250);
+        return false;
+      }
     }
-    if (tru && clientHeight + DOMTop.offsetTop + DOMTop.clientHeight < clientHeight + scrollTop) {
-      DOMTop.setAttribute('class', 'page-nav top');
-      tru = false
-    }
-    if (clientHeight + DOMTop.offsetTop + DOMTop.clientHeight - 100 > clientHeight + scrollTop) {
-      DOMTop.setAttribute('class', 'page-nav');
-      tru = true
-    }
-  }, false)
+  });
 }
 
 window.onload = init;
